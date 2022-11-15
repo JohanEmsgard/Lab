efadb3bac01a0336e9b3e9767645af0647a30501
Johanemsgard <emsgard.johan@elev.ga.ntig.se>
2022-11-15 13:09:13 +0100

    bjkcasbhjcasbh

--- Lab/Program.cs
+++ Lab/Program.cs
@@ -1,12 +1,12 @@
 ﻿using RestSharp;
 
 RestClient pokeClient = new RestClient("https://pokeapi.co/api/v2/");
-
 RestRequest request = new("pokemon/sceptile");
-
 RestResponse response = pokeClient.GetAsync(request).Result;
 
-Console.WriteLine(response.StatusCode);
+
+
+Console.WriteLine(response.Content);
 
 Console.ReadLine();
 
