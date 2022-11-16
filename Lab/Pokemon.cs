a86f06b00365a206cf55443c4313080402d822f5
Johanemsgard <emsgard.johan@elev.ga.ntig.se>
2022-11-16 12:29:44 +0100

    bmhdsfhbjd

--- Lab/Pokemon.cs
+++ Lab/Pokemon.cs
@@ -1,7 +1,15 @@
+using System.Text.Json.Serialization;
 public class Pokemon
 {
+    [JsonPropertyName("name")]
     public string name { get; set; }
+
+    [JsonPropertyName("weight")]
     public int weight { get; set; }
+
+    [JsonPropertyName("isdefault")]
     public bool is_default { get; set; }
+
+    [JsonPropertyName("height")]
     public int height { get; set; }
 }
