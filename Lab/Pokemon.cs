46d2050b11c73d291e17695806f7ec4e03245b9e
Johanemsgard <emsgard.johan@elev.ga.ntig.se>
2022-11-16 12:27:22 +0100

    bhjscdhjsd

---
+++ Lab/Pokemon.cs
@@ -0,0 +1,7 @@
+public class Pokemon
+{
+    public string name { get; set; }
+    public int weight { get; set; }
+    public bool is_default { get; set; }
+    public int height { get; set; }
+}
--- Lab/Program.cs
+++ Lab/Program.cs
@@ -1,12 +1,27 @@
 ﻿using RestSharp;
-
+using System.Text.Json;
+using System.Net;
 RestClient pokeClient = new RestClient("https://pokeapi.co/api/v2/");
 RestRequest request = new("pokemon/sceptile");
 RestResponse response = pokeClient.GetAsync(request).Result;
 
 
+if (response.StatusCode == HttpStatusCode.OK)
+{
+Pokemon p = JsonSerializer.Deserialize<Pokemon>(response.Content);
+Console.WriteLine(p.name);
+Console.WriteLine(p.weight); 
+Console.WriteLine(p.height);
+}
 
-Console.WriteLine(response.Content);
+else
+{
+    Console.WriteLine("WHAT THE FUCK?!?!?!?!");
+}
+
+
+
+//Console.WriteLine(response.Content);
 
 Console.ReadLine();
 
